592cb60b402055230bccce6d8ffab2e7dd7b3cdb
Eric J. Had Arach <hadaracheric@gmail.com>
2022-08-10 08:31:27 -0300

    Terminado

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(cadena) {
+    let arreglo = cadena.split('');
+    let nuevaCadena = '';
+    let indiceArreglo = 0;
+    for(let i = cadena.length-1; i>=0; i--) {
+        arreglo[indiceArreglo] = cadena[i];
+        indiceArreglo++;
+    }
+    nuevaCadena = arreglo.join('');
+    return nuevaCadena;
 };
 
 // Do not edit below this line
