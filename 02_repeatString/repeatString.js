06b9008c602f526ff82b10ecb70e6e45fc3ffaf3
Eric J. Had Arach <hadaracheric@gmail.com>
2022-08-10 08:11:33 -0300

    Termiado

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(cadena, repeticiones) {
+    let nuevaCadena = '';
+    if(repeticiones < 0) {
+        return 'ERROR';
+    }
+    if(cadena === '') {
+        return '';
+    }
+    for(let i=1; i<=repeticiones; i++) {
+        nuevaCadena += cadena;
+    }
+    return nuevaCadena;
 };
 
 // Do not edit below this line
