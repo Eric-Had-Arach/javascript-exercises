90ea1466ce0609a0778963a63f6509e4672a9773
Eric J. Had Arach <hadaracheric@gmail.com>
2022-08-11 09:52:09 -0300

    Terminado

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(tempFahrenheit) {
+    let temperatura = ((tempFahrenheit - 32) * 5/9);
+    if (Number.isInteger(temperatura)) {
+        return temperatura;
+    }else {
+        return parseFloat(temperatura.toFixed(1));
+    }
 };
 
-const ctof = function() {
-
+const ctof = function(tempCelsius) {
+    let temperatura = (tempCelsius * 9/5 + 32);
+    if (Number.isInteger(temperatura)) {
+        return temperatura;
+    }else {
+        return parseFloat(temperatura.toFixed(1));
+    }
 };
 
 // Do not edit below this line
