d0965ef247cea6e7503b185d736e7d2cedb0ca09
Eric J. Had Arach <hadaracheric@gmail.com>
2022-08-11 10:05:27 -0300

    Terminado

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arreglo) {
+	let resultado = 0;
+    for(let valor of arreglo) {
+        resultado += valor;
+    }
+    return resultado;
 };
 
-const multiply = function() {
-
+const multiply = function(arreglo) {
+    let resultado = 1;
+    for(let valor of arreglo) {
+        resultado *= valor;
+    }
+    return resultado;
 };
 
-const power = function() {
-	
+const power = function(base, exponente) {
+	let resultado = 1;
+    for(let i = 1; i <= exponente; i++) {
+        resultado *= base;
+    }
+    return resultado;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let resultado = 1;
+    for(let i = num; i >= 1; i--) {
+        resultado *= i;
+    }
+    return resultado;
 };
 
 // Do not edit below this line
