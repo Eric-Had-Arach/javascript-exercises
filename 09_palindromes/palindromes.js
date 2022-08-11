df3537c058a8f19734637161796e407071a8d30b
Eric J. Had Arach <hadaracheric@gmail.com>
2022-08-11 11:03:00 -0300

    Terminado

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,30 @@
-const palindromes = function () {
+//ignorar mayúsculas y minúsculas
+//ignorar signos de puntuación y espacios
 
+const palindromes = function (cadena) {
+    const cadenaMinusculas = cadena.toLowerCase();
+    let arregloSinSimbolos = [];
+    let copiaArregloSinSimbolos = [];
+    let arregloInvertido = [];
+    for (let valor of cadenaMinusculas) {
+        if ((valor.charCodeAt() >= 97 && valor.charCodeAt() <= 122) ||
+            (valor.charCodeAt() === 225 || valor.charCodeAt() === 233 || 
+             valor.charCodeAt() === 237 || valor.charCodeAt() === 243 || 
+             valor.charCodeAt() === 250)) {
+            arregloSinSimbolos.push(valor);
+            copiaArregloSinSimbolos.push(valor);    
+        }
+    }
+    arregloInvertido = copiaArregloSinSimbolos.reverse();
+    for (let i = 0; i < arregloSinSimbolos.length; i++) {
+        if (arregloSinSimbolos[i] !== arregloInvertido[i]) {
+            return false;
+        }
+    }
+    return true;
 };
+
+palindromes('Racecar!');
 
 // Do not edit below this line
 module.exports = palindromes;
