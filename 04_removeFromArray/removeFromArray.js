cb9d1dec6d30264457c91a1e2eb7ac601885f80c
Eric J. Had Arach <hadaracheric@gmail.com>
2022-08-11 08:04:26 -0300

    Terminado

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arreglo, ...valores) {    
+    for(let i=0; i<valores.length; i++) {
+        for(let j=0; j<arreglo.length; j++) {
+            if(arreglo[j] === valores[i]) {
+                arreglo.splice(j, 1);
+            }
+        }
+    }
+    return arreglo;
 };
 
 // Do not edit below this line
