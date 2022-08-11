cfa41f597312f1bde1df99da59c977c93712a32c
Eric J. Had Arach <hadaracheric@gmail.com>
2022-08-11 09:27:25 -0300

    Terminado

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(anio) {
+    if (anio % 4 === 0 && (anio % 100 !== 0 || (anio % 100 === 0 && anio % 400 === 0))) {
+        return true;
+    }else {
+        return false;
+    }
 };
 
 // Do not edit below this line
