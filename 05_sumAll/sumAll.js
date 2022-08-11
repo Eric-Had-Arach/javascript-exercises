1ae4106331680dc6dadd8eac79a0c04e0ea7ff66
Eric J. Had Arach <hadaracheric@gmail.com>
2022-08-11 08:51:22 -0300

    Terminado

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let resultado = 0;
+    if(typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'ERROR';
+    }
+    if(num1<0 || num2<0) {
+        return 'ERROR';
+    }
+    if(num1<=num2) {
+        for(let i=num1; i<=num2; i++) {
+            resultado += i;
+        }
+        return resultado;
+    }
+    if(num1>num2) {
+        for(let i=num1; i>=num2; i--) {
+            resultado += i;
+        }
+        return resultado;
+    }
 };
 
 // Do not edit below this line
