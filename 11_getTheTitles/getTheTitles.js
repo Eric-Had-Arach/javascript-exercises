c3fd9f684693d2ea6e7e77f2f7b191c3b43e0a87
Eric J. Had Arach <hadaracheric@gmail.com>
2022-08-24 09:38:58 -0300

    getTheTitles terminado

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arreglo) {
+    return arreglo.map(function(elemento) {
+        return elemento.title;
+    });
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
