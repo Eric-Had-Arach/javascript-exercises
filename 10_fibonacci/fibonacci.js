1b1a920a50e9657beb9070456ca64075e600aa19
Eric J. Had Arach <hadaracheric@gmail.com>
2022-08-24 09:09:36 -0300

    Fibonacci terminado

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
-
+const fibonacci = function(posicion) {
+    const serie = [1, 1];
+    if (typeof posicion === 'string') {
+        posicion = Number(posicion);
+        if (isNaN(posicion)) {
+            return 'OOPS';
+        }
+    }
+    if (posicion <= 0) {
+        return 'OOPS';
+    }
+    if (posicion === 1) {
+        return serie[0];
+    }
+    if (posicion === 2) {
+        return serie[1];
+    }
+    if (posicion >= 3) {
+        for (let i = 2; i < posicion; i++) {
+            serie.push(serie[i-2] + serie[i-1]);
+            if (i === posicion - 1) {
+                return serie[i];
+            }
+        }
+    }
 };
 
 // Do not edit below this line
