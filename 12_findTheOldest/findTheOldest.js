45b08c7dc26f726bed9cf145884323a4639fa113
Eric J. Had Arach <hadaracheric@gmail.com>
2022-08-25 19:39:54 -0300

    Terminado

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const getAge = function(yearOfDeath, yearOfBirth) {
+    if (!yearOfDeath) {
+        yearOfDeath = new Date().getFullYear();
+    }
+    return yearOfDeath - yearOfBirth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce(function(older, currentPerson) {
+        let olderAge = getAge(older.yearOfDeath, older.yearOfBirth);
+        let ageCurrentPerson = getAge(currentPerson.yearOfDeath, currentPerson.yearOfBirth);
+        if (olderAge < ageCurrentPerson) {
+            return currentPerson;
+        }
+        else {
+            return older;
+        }
+    });
 };
 
 // Do not edit below this line
